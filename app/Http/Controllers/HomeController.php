19b00d4774b116944adffe3602d58ac4378957c9
Amir-16 <greenfitter516@gmail.com>
2020-12-15 10:38:04 +0600

    login

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-
+use Auth;
 class HomeController extends Controller
 {
     /**
@@ -24,5 +24,19 @@ class HomeController extends Controller
     public function index()
     {
         return view('home');
+    }
+
+    public function logout()
+    {
+      // Auth::logout();
+       //return view('welcome');
+
+       Auth::logout();
+       $notification=array(
+         'message' => 'successfully logout',
+         'alert-type' =>'success'
+       );
+       return Redirect()->route('login')->with($notification);
+
     }
 }
